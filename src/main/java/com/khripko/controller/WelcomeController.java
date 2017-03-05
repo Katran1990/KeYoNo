b62ad9a4bbc4c32b1025ccf3add4be9a08e5a8b1
Katran1990 <boris.katran@gmail.com>
2017-03-05 22:02:42 +0200

    sign up and sign in forms have been added

--- src/main/java/com/khripko/controller/WelcomeController.java
+++ src/main/java/com/khripko/controller/WelcomeController.java
@@ -1,12 +1,48 @@
 package com.khripko.controller;
 
+import com.khripko.model.UserDetails;
+import com.khripko.service.UserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDate;
 
 @Controller
 public class WelcomeController {
+
+    @Autowired
+    UserDetailsService userDetailsService;
+
+
     @GetMapping("/login")
-    public String getLoginPage(){
-        return "login";
+    public String getSigninPage(){
+        return "sign-in";
+    }
+
+    @GetMapping("/registration")
+    public String getSignupPage(){
+        return "sign-up";
+    }
+
+    @PostMapping("/post-user-info")
+    public String sendUserRegistration(@RequestParam String login, @RequestParam String firstName,
+                                       @RequestParam String lastName, @RequestParam String email,
+                                       @RequestParam String confirmationPassword, @RequestParam LocalDate birthDate,
+                                       @RequestParam String gender){
+        UserDetails user = new UserDetails();
+        user.setLogin(login);
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setEmail(email);
+        user.setBirthDate(birthDate);
+        user.setCreationDate(LocalDate.now());
+        user.setPassword(confirmationPassword);
+        user.setSex(gender);
+        user.setActive(false);
+        userDetailsService.create(user);
+        return "redirect:/welcome";
     }
 }
