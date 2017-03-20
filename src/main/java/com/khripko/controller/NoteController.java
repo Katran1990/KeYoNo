9a2efeae832ae360178598dffd77ae02208e3f47
Katran1990 <boris.katran@gmail.com>
2017-03-20 17:27:32 +0200

    users_page has been added

--- src/main/java/com/khripko/controller/NoteController.java
+++ src/main/java/com/khripko/controller/NoteController.java
@@ -7,7 +7,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 @Controller
 public class NoteController {
@@ -17,13 +16,7 @@ public class NoteController {
 
     @GetMapping("/notes")
     public String getNotesPage(Model model, HttpServletRequest request){
-        HttpSession session = request.getSession();
-        while (session.getAttributeNames().hasMoreElements()){
-            if(session.getAttributeNames().nextElement().equals("currentUserId")){
-                model.addAttribute("notes", noteService.readAll());
-                return "notes";
-            }
-        }
-        return "redirect:/login";
+        model.addAttribute("notes", noteService.readAll());
+        return "notes";
     }
 }
--- src/main/java/com/khripko/controller/UserController.java
+++ src/main/java/com/khripko/controller/UserController.java
@@ -1,47 +1,22 @@
 package com.khripko.controller;
 
-import com.khripko.model.UserDetails;
 import com.khripko.service.UserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import java.util.List;
 
 @Controller
 public class UserController {
 
     @Autowired
-    UserDetailsService userDetailsService;
-
-    @PostMapping("/check-user")
-    public String checkUser(@RequestParam("login") String login, @RequestParam("password") String password, HttpServletRequest request){
-        if (login==null||password==null){
-            return "redirect:/login";
-        }
-        Long userId = userDetailsService.getUserIdIfExist(login);
-        if (userId==-1){
-            return "redirect:/login";
-        }
-        UserDetails user = userDetailsService.read(userId);
-        if (!user.getPassword().equals(password)){
-            return "redirect:/login";
-        }
-        HttpSession session = request.getSession();
-        session.setAttribute("currentUserId", user.getId());
-        return "redirect:/users";
-    }
-
+    UserDetailsService userService;
 
     @GetMapping("/users")
-    public String getIndex(Model model, HttpServletRequest request){
-        List<UserDetails> users = userDetailsService.readAll();
-        model.addAttribute(users);
+    public String getNotesPage(Model model, HttpServletRequest request){
+        model.addAttribute("users", userService.readAll());
         return "users";
     }
 }
