26b8ef1b5b6adceca4cf184247deb8308e76d796
Katran1990 <boris.katran@gmail.com>
2017-03-20 17:51:14 +0200

    profile page has been added

---
+++ src/main/java/com/khripko/controller/ProfileController.java
@@ -0,0 +1,13 @@
+package com.khripko.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProfileController {
+
+    @GetMapping("/profile")
+    public String getProfile(){
+        return "profile";
+    }
+}
