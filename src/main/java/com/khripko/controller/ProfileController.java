df0d109b5ec6dea47c1658a8490d2e27ec7c738a
Katran1990 <boris.katran@gmail.com>
2017-03-24 21:56:00 +0200

    profile controller and template have been modified

--- src/main/java/com/khripko/controller/ProfileController.java
+++ src/main/java/com/khripko/controller/ProfileController.java
@@ -1,13 +1,24 @@
 package com.khripko.controller;
 
+import com.khripko.model.UserDetails;
+import com.khripko.service.UserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class ProfileController {
 
+    @Autowired
+    UserDetailsService userService;
+
     @GetMapping("/profile")
-    public String getProfile(){
+    public String getProfile(HttpServletRequest request, Model model){
+        model.addAttribute("user", userService.read((Long) request.getSession().getAttribute("currentUserId")));
         return "profile";
     }
 }
